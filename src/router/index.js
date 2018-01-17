19d5f8e59dbb9e97b931d0f0691c84fa235029b8
zhangtao <328480460@qq.com>
2018-01-17 17:29:58 +0800

    刷新不会丢失数据

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home,
+      redirect: '/home/production/productInfo',
       children: [
         ...Production,
         ...Node,
