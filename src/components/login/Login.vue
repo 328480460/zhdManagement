fdc7a969766d86d6461235b74d82dea1ab3ca140
zhaoyu <767700694@qq.com>
2018-02-24 14:32:08 +0800

    【节点】列表查询的“节点分类、类型”替换完成

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -87,6 +87,7 @@ export default {
               console.log("login的userid---"+JSON.stringify(res.data.user.id))
               // 命名的路由
               this.$router.push({ name: 'Home', params: { userId: 'userIdTest' }})
+
               //记住密码
               if(this.checked == true){
                 localStorage.setItem('account',JSON.stringify(this.accountName))
